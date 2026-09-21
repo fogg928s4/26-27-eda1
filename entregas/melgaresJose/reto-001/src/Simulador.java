2db23264d6c358858d0c76e5563cc52996e5bf3e
Jose Melgares <rjmzelaya@gmail.com>
2026-09-21 08:21:47 +0200

    feat: makes time go forward

--- entregas/melgaresJose/reto-001/src/Simulador.java
+++ entregas/melgaresJose/reto-001/src/Simulador.java
@@ -1,10 +1,32 @@
 class Simulador {
+
+    private int horas;
+    private int minutos;
+    private final int DURACION_HORAS = 4;
+    
+    private Fila laFila;
+
     public static void main(String[] args) {
         iniciarSimulacion();
     }
 
     private static void iniciarSimulacion() {
-        Fila laFila = new Fila();
+        horas = 0;
+        minutos = 0;
+        laFila = new Fila();
 
+        while (horas < DURACION_HORAS) {
+            avanzarTiempo();
+        }
+    }
+
+    private static void avanzarTiempo() {
+        final int MINUTOS_EN_HORA = 60;
+        minutos = minutos + 1;
+        
+        if(minutos == MINUTOS_EN_HORA ) {
+            minutos = 0;
+            horas = horas + 1;
+        }
     }
 }
