b6e2cca1561dd964b43663c0c43e0ca757e9014e
Jose Melgares <rjmzelaya@gmail.com>
2026-09-24 20:16:03 +0200

    refactor: makes Fila a list of linked Persona elements

--- entregas/melgaresJose/reto-001/src/Fila.java
+++ entregas/melgaresJose/reto-001/src/Fila.java
@@ -1,22 +1,44 @@
 class Fila {
 
     private int longitud;
+    private Persona primeroEnFila;
 
     public Fila(int largoInicial) {
-        longitud = largoInicial;
+        this.longitud = 0;
+        this.primeroEnFila = null;
+        for (int i = 0; i < largoInicial; i++) {
+            aumentarFila(new Persona());
+        }
     }
 
     public Fila() {
         this(0);
     }
 
-    public void aumentarFila() {
+    public void aumentarFila(Persona siguienteEnFila) {
         this.longitud++;
+        if (primeroEnFila == null) {
+            primeroEnFila = siguienteEnFila;
+        } else {
+            Persona actual = primeroEnFila;
+            while (actual.getDetrasDe() != null) {
+                actual = actual.getDetrasDe();
+            }
+            actual.colocarDetrasDe(siguienteEnFila);
+        }
+    }
+
+    public void aumentarFila() {
+        if (Math.random() < 0.6) {
+            aumentarFila(new Persona());
+        }
     }
 
     public void mostrarFila() {
-        for (int i = 0; i < longitud; i++) {
+        Persona actual = primeroEnFila;
+        while (actual != null) {
             System.out.print(" ☻ ");
+            actual = actual.getDetrasDe();
         }
         System.out.println();
     }
---
+++ entregas/melgaresJose/reto-001/src/Persona.java
@@ -0,0 +1,16 @@
+class Persona {
+
+    private Persona detrasDe;
+
+    public Persona() {
+        detrasDe = null;
+    }
+
+    public Persona(Persona enFrente) {
+        detrasDe = enFrente;
+    }
+
+    public void colocarDetrasDe(Persona enFrente) {
+        detrasDe = enFrente;
+    }
+}
